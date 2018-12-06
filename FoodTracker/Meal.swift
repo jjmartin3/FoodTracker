7279556740d33f12c88e1ccc69ed20ff6337b490
jjmartin3 <jjmartin@ProObject.com>
2018-12-05 22:03:05 -0500

    Add files via upload

---
+++ FoodTracker/Meal.swift
@@ -0,0 +1,81 @@
+//
+//  Meal.swift
+//  FoodTracker
+//
+//  Created by John Martin on 11/11/18.
+//  Copyright © 2018 ProObject. All rights reserved.
+//
+
+import UIKit
+import os.log
+
+class Meal: NSObject, NSCoding {
+    
+    //MARK: Properties
+    
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    struct PropertyKey {
+        static let name = "name"
+        static let photo = "photo"
+        static let rating = "rating"
+    }
+    
+    //MARK: Archiving Paths
+    
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("meals")
+
+    //MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+        
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+        
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        // Log some things.
+        //os_log("Here is an default message.", log: OSLog.default, type: .default)
+        //os_log("Here is an info message.", log: OSLog.default, type: .info)
+        //os_log("Here is a debug message.", log: OSLog.default, type: .debug)
+        //os_log("Here is an error message.", log: OSLog.default, type: .error)
+        //os_log("Here is a fault message.", log: OSLog.default, type: .fault)
+    }
+    
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(rating, forKey: PropertyKey.rating)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+    
+        // The name is required. If we cannot decode a name string, the initializer should fail.
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String else {
+            os_log("Unable to decode the name for a Meal object.", log: OSLog.default, type: .debug)
+            return nil
+        }
+        
+        // Because photo is an optional property of Meal, just use conditional cast.
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        
+        let rating = aDecoder.decodeInteger(forKey: PropertyKey.rating)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
+    }
+}
